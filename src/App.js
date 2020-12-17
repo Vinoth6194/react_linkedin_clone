7e7504e34204e389fee9f576bf63fdf22dbba9e9
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-18 01:10:36 +0530

    header setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Linkedin Clone</h1>
+      {/* Header */}
+      <Header />
+      {/* AppBody */}
+      {/* sidebar */}
+      {/* feesd */}
+      {/* widgets */}
     </div>
   );
 }
