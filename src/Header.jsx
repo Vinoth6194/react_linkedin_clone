52258819a1dac13cc98f9e9f2afdfb900142f157
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-18 01:37:07 +0530

    search icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <h1>header</h1>
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
 }
 
 export default Header;
