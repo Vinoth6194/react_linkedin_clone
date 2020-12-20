1a7f86eac46082a6f6ef6fa89b85b21828b470fd
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-21 02:32:46 +0530

    persisted the user details to the browser for refresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { selectuser } from "./features/userSlice";
+import { login, selectuser } from "./features/userSlice";
 import Feed from "./Feed";
+import { auth } from "./firebase";
 import Header from "./Header";
 import Login from "./Login";
 import Sidebar from "./Sidebar";
 function App() {
   const user = useSelector(selectuser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      }
+    });
+  }, []);
   return (
     <div className="app">
       {/* <h1>Linkedin Clone</h1> */}
