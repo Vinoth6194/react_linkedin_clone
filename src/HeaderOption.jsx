41935d5263889b511b31dae687314a5dff0e1101
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-23 02:36:54 +0530

    header avatar based on name

--- src/HeaderOption.jsx
+++ src/HeaderOption.jsx
@@ -1,12 +1,17 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./headeroption.css";
 function HeaderOption({ avatar, Icon, title, onClick }) {
+  const user = useSelector(selectUser);
   return (
     <div className="headerOption">
       {/* //*the && stands for If like condition in react  */}
       {Icon && <Icon className="headerOption__icon" />}
-      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
+      {avatar && (
+        <Avatar className="headerOption__icon">{user?.email[0]} </Avatar>
+      )}
       <h3 className="headerOption__title" onClick={onClick}>
         {title}{" "}
       </h3>
