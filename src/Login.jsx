ee002913df942af3aa6eafdaa48ed540b858e1fc
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-23 01:01:43 +0530

    login logo styled

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./login.css";
 function Login() {
-  return <div>Not logged in</div>;
+  return (
+    <div className="login">
+      <img
+        src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+        alt=""
+      ></img>
+    </div>
+  );
 }
 
 export default Login;
