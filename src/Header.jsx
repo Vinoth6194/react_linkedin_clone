dc8b64f8524b7cf46ce7beeac28778c45a95a48a
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-23 01:11:45 +0530

    search boxplaceholder

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
         />
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder="Search" />
         </div>
       </div>
       <div className="header__right">
