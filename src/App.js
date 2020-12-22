dcdfcc3d6a904f6d4fa41a2ffc7e362e27c5e990
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-23 00:54:22 +0530

    condition added for login component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Login from "./Login";
+import { selectUser } from "./features/userSlice";
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       {/* <h1>Linkedin Clone</h1> */}
       {/* Header */}
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* widgets */}
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./login.css";
+function Login() {
+  return <div>Not logged in</div>;
+}
+
+export default Login;
