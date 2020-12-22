2bc41855354b6ab246589aea721c46f479a6de38
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-23 02:23:26 +0530

    user details are updated as per the loggin details in the sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,10 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./sidebar.css";
 function Sidebar() {
+  const user = useSelector(selectUser);
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -15,9 +18,9 @@ function Sidebar() {
           src="https://images.unsplash.com/photo-1523961738618-3c1dde78b4c6?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fGNvbG9yZnVsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
           alt=""
         />
-        <Avatar className="sidebar__avatar"></Avatar>
-        <h2>Vinothkumar</h2>
-        <h4>linkedin.vino@gmail.com</h4>
+        <Avatar className="sidebar__avatar">{user.email[0]}</Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
       <div className="sidebar__stats">
         <div className="sidebar__stat">
